c4ca8a87d00d01af7531c508418b6aa13b144d2b
Nika <70407540+NikaAlgorithm@users.noreply.github.com>
2025-04-26 23:54:05 +0330

    Add files via upload
    Solution of https://codeforces.com/group/5AhnQzBd7I/contest/368488/problem/C

---
+++ Codeforces/position.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int n, a, b;
+	cin >> n >> a >> b;
+
+	int result;
+	
+	if (a + 1 >= n - b){
+		result = a + 1;
+	}
+	if (a + 1 < n - b){
+		result = n -b;
+	}
+
+	result = n - result + 1;
+
+	cout << result << endl;
+
+	return 0;
+
+}
+
